a4466fb9433d5fc6c6d1274bf88e6cac420171ce
YamaguchiYamato <messala2319@gmail.com>
2020-07-03 14:19:17 +0900

    コンポーネント間でのデータ渡し todo追加処理実装

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+//親コンポーネント
 const TodoApp = () => {
   const [value, setValue] = useState("");
   const [todoList, setTodoList] = useState([]);
@@ -13,8 +14,9 @@ const TodoApp = () => {
 
   const add = () => {
     //setTodoListの配列の先頭にvalueの値が追加される。
-    const newTodo = [...todoList, value];
-    setTodoList(newTodo);
+    const newTodo = {id: todoList.length, content: value};
+    const newTodoList = [...todoList, newTodo];
+    setTodoList(newTodoList);
 
     //実行後にsetValueが空に置き換わる。
     setValue("");
@@ -24,12 +26,33 @@ const TodoApp = () => {
   return (
     <div>
       <h1>TODO App</h1>
-      <input type="text" value={value} onChange={handleChange} />
+      <div>
+        <AddTodo onChange={handleChange} add={add} />
       <ul>
-        {todoList.map((todo, i) => (
-          <li key={i}>{todo}</li>
-        ))}
-      </ul>
+        {todoList.map(todo => (
+          <TodoElement key={todo.id} content={todo.content} />
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
+
+const AddTodo = props => {
+  return (
+    <div>
+      <input type="text" value={props.value} onChange={props.onChange} />
+      <button onClick={props.add}>追加</button>
+    </div>
+  );
+};
+
+//子コンポーネント
+//引数props
+const TodoElement = props => {
+  return (
+    <li>
+      {props.content}
+    </li>
+  )
+}
